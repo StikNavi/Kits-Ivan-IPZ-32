7fab10f40ca5c926f56056ca307d8ac5885b09e7
SterkGame <danilkokosta@gmail.com>
2023-12-15 19:09:22 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ menuBtn.addEventListener('click', function () {
   menu.classList.toggle('active_menu');
   menuBtn.classList.toggle('active_menu');
 })
+
+function filterItems(category) {
+  var items = document.querySelectorAll('#itemList li');
+
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    
+    if (category === 'all' || item.classList.contains(category)) {
+      // item.style.display = 'list-item';
+      item.classList.remove('hidden-card');
+    } else {
+      // item.style.display = 'none';
+      item.classList.add('hidden-card');
+    }
+  }
+}
